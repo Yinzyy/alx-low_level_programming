222193afef8de3d5d1f91716908079965ea49e5b
Yinzyy <onyinyechidinmagift@gmail.com>
2022-10-14 19:50:43 -0700

    5-print

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,22 +1,20 @@
-#include <stdio.h>                                                                                                            
-#include <stdlib.h>                                                                                                           
-#include <time.h>                                                                                                             
-                                                                                                                       
-/**                                                                                                                           
- * main - Entry point                                                                                                         
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
- * Return: Always 0 (Success)                                                                                                 
- */                                                                                                                           
+ * Return: Always 0.
+ */
 
-int main(void)                                                                                                                
-{                                                                                                                             
-	int i;                                                                                                                
+int main(void)
+{
+	int num;
 
-	for (i = 0; i < 10; i++)                                                                                              
-		printf("%d", i);                                                                                              
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
 
-	printf("\n");                                                                                                         
-	
-	return (0);                                                                                                           
+	printf("\n");
 
-} 
+	return (0);
+
+}
