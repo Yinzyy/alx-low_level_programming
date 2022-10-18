d5921b3b0e7fc3e899b2cc49a161b3206d824aa0
Yinzyy <onyinyechidinmagift@gmail.com>
2022-10-18 12:50:35 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - Prints "_putchar" followed by a new line.
+ * main - Prints _putchar followed by a new line.
  *
- * Return: Always 0.
+ * Return: Always: 0
  */
 
 int main(void)
