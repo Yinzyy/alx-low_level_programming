0b2800d020fa320de2b9f13015e92ea84535f92d
Yinzyy <onyinyechidinmagift@gmail.com>
2022-10-20 16:48:57 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,18 @@
  */
 void print_most_numbers(void)
 {
-	int i = '0'
-		;
-	for (; i <= '9'; i++)
+	int x = 0;
+
+	for (; x <= 9; x++)
 	{
-		if (i != '2' && i != '4')
-			_putchar(i);
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
 	}
 	_putchar('\n');
 }
